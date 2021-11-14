8872e87dd5c368ad23ac9ce11f005c9932517780
Ellandula-Vamshi <93942576+Ellandula-Vamshi@users.noreply.github.com>
2021-11-14 21:20:54 +0530

    Add files via upload

---
+++ HibernateCrudDemo/src/Pogo/Employee.java
@@ -0,0 +1,28 @@
+package Pogo;
+
+public class Employee {
+
+	private Integer id;
+	private String name;
+	private String sal;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSal() {
+		return sal;
+	}
+	public void setSal(String sal) {
+		this.sal = sal;
+	}
+	
+	
+}
