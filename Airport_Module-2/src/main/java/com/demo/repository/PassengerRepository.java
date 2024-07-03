3d54de0116aa4b22fb13f883d542718b1b8fa45e
Ellandula-Vamshi <93942576+Ellandula-Vamshi@users.noreply.github.com>
2024-07-03 00:56:40 -0500

    Delete Airport_Module-2 directory

--- Airport_Module-2/src/main/java/com/demo/repository/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.pojo.Passengers;
-
-public interface PassengerRepository extends JpaRepository<Passengers, Integer>{
-
-}
--- Airport_Module-2/src/main/java/com/demo/repository/SchduleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.pojo.Schedule;
-
-public interface SchduleRepository extends JpaRepository<Schedule, Integer>{
-
-}
